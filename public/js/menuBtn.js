7a89c1616a77a4e8eb057261348c477ba906addc
naikds <yistmmy1284@gmail.com>
2025-12-29 20:34:50 +0900

    Update menuBtn.js

--- public/js/menuBtn.js
+++ public/js/menuBtn.js
@@ -17,6 +17,7 @@ export function setMenuBtn(){
 
         //メニューを表示する
         let selectItem=null;
+        let isDown = false;
         menuBtn.addEventListener('pointerdown', (e) => {
             e.preventDefault();
             selectItem=null;
@@ -24,10 +25,11 @@ export function setMenuBtn(){
         });
 
         //メニューで選ばれた項目の色を変える
-        menuBtn.addEventListener('touchmove', (e) => {
+        menuBtn.addEventListener('pointermove', (e) => {
+            if(!isDown) return;
             e.preventDefault();
-            const touchY = e.changedTouches[0].clientY;
-            const touchX = e.changedTouches[0].clientX;
+            const touchY = e..clientY;
+            const touchX = e..clientX;
             selectItem=null;
             const menuItems = document.querySelectorAll('.menu-item');
             menuItems.forEach(item => {
@@ -50,7 +52,7 @@ export function setMenuBtn(){
             if(selectItem != null){
                 selectItem.style.backgroundColor = '#0078d4';
                 if(selectItem.classList.contains('menuBtnMenu')){
-                    btnAct(selectItem, e.target.id);
+                    //btnAct(selectItem, e.target.id);
                 }else if(!selectItem.classList.contains('submenu-item')){
                     submenus.forEach(sub =>{sub.style.display = 'none';});
                 }
@@ -58,16 +60,17 @@ export function setMenuBtn(){
         });
 
         //メニューで選ばれた項目の処理を実行する
-        menuBtn.addEventListener('touchend', (e) => {
+        menuBtn.addEventListener('pointerup', (e) => {
+            isDown = false;
             e.preventDefault();
             preDataSave();
             if(selectItem){
-                const touchY = e.changedTouches[0].clientY;
-                const touchX = e.changedTouches[0].clientX;
+                const touchY = e.clientY;
+                const touchX = e.clientX;
                 
                 const rect = selectItem.getBoundingClientRect();
                 if (touchY >= rect.top && touchY <= rect.bottom && touchX >= rect.left && touchX <= rect.right) {
-                    btnAct(selectItem, e.target.id);
+                    //btnAct(selectItem, e.target.id);
                 }
             }
 
